7731ff832bfc3eecb862a7d796dd1320c25a10b4
fadedrifleman <fadedrifleman@gmail.com>
2022-05-05 15:51:47 +0530

    basic structure created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.createRoot(<h1>Hello World</h1> ,document.getElementById('root'));
+
+ReactDOM.render(<App /> ,document.getElementById('root'));
 
