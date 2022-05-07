de78cad74f6c35c54c5994d12affa6067d69b393
fadedrifleman <fadedrifleman@gmail.com>
2022-05-07 10:00:22 +0530

    completed functionality

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 function Note(props) {
-    return (
-        <div className="note">
-            <h1>{props.title}</h1>
-            <p>{props.content}t</p>
-        </div>
-    );
+
+  function handleClick(event) {
+    props.onDelete(props.id)
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-ReactDOM.render(<App /> ,document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
